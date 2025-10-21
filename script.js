3e7c67c457aafd703d8e8c501653cf5b0c5bc4fb
SadikRa <sadikrahman494@gmail.com>
2025-10-21 19:28:21 +0600

    updated

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
-
 // console.log('hello sadik');
 
-const names = [
-    'sadik',
-    'also sadik',
-    'yah, sadik',
-    'okh, sadik'
-]
+const names = ["sadik", "also sadik", "yah, sadik", "okh, sadik"];
 
 // console.log(names);
+
+// console.log(undefined === null);
+
+// console.log(['object'] == 'object');
+
+// &&
+// ||
+// !
+
+const user = {
+  id: 1,
+  name: "Amir",
+  job: "Actor",
+};
+const userJSON = JSON.stringify(user);
+console.log(userJSON);
