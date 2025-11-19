f2a19bab56a15ae0536228582f13772153850f52
SadikRa <sadikrahman494@gmail.com>
2025-11-19 20:22:12 +0600

    Function Composition aded done

--- leetCode.js
+++ leetCode.js
@@ -76,3 +76,15 @@ var reduce = function (nums, fn, init) {
   }
   return val;
 };
+
+// 2629. Function Composition
+
+var compose = function (functions) {
+  return function (x) {
+    let result = x;
+    for (let i = functions.length - 1; i >= 0; i--) {
+      result = functions[i](result);
+    }
+    return result;
+  };
+};
